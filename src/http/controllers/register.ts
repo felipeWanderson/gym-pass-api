d10f1db49f12093818edd51ed3ba6d8f9ab7a2ce
Felipe Leal <felipewanderson2020@gmail.com>
2024-03-20 18:27:21 -0300

    fix: replace folders

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,8 +1,8 @@
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/use-case/errors/user-already-exists-error'
+import { RegisterUseCase } from '@/use-case/register'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
-import { RegisterUseCase } from '../use-case/register'
-import { PrismaUsersRepository } from '../repositories/prisma/prisma-users-repository'
-import { UserAlreadyExistsError } from '../use-case/errors/user-already-exists-error'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
